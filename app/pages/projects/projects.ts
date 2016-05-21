5681d2b9c18232fe1adf1b6031d30c5e09b5d302
David Zheng <david.zheng@cogeco.com>
2016-05-21 15:05:14 -0400

    a

--- app/pages/projects/projects.ts
+++ app/pages/projects/projects.ts
@@ -39,15 +39,8 @@ export class ProjectsPage implements OnInit {
 
     getResult(url) {
 
-        //let parms = { id: "1", type: "Test" };
-        //this.getProjects();
-
-<<<<<<< HEAD
-=======
-        
->>>>>>> 42cd15b435ef450e67ddb2c8e8db148a279e1c50
-
-        this.mapleRestData.load(url,this.parms).subscribe(
+      
+       this.mapleRestData.load(url,this.parms).subscribe(
             data => { this.projects = data; console.log(this.projects); }
         );
 
