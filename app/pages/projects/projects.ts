fac051fd0c6006d7fcb566ee9b485246d349c63a
David Zheng <david.zheng@cogeco.com>
2016-05-22 21:10:16 -0400

    projects page with ion-card and style

--- app/pages/projects/projects.ts
+++ app/pages/projects/projects.ts
@@ -2,6 +2,7 @@ import {Page, NavController, NavParams} from 'ionic-angular';
 import {OnInit} from 'angular2/core';
 import {MapleRestData} from '../../providers/maple-rest-data/maple-rest-data';
 import {MAPLECONF} from '../../providers/maple-rest-data/maple-config';
+import {ProjectDetailPage} from '../project-detail/project-detail';
 
 //projects: Object;
 
@@ -42,20 +43,14 @@ export class ProjectsPage implements OnInit {
     }
 
     getResult(url) {
-
-
         this.mapleRestData.load(url, this.parms).subscribe(
             data => { this.projects = data; console.log(this.projects); }
         );
-
-        //this.mapleRestData.load(url, parms).then(projects => this.projects = projects); //This generate projects type is not defined error
-        //this.mapleRestData.projectStatic().then(projects => this.projects = projects);
-
-        // this.projects = {id: "Static ID from function", name: "Static Name from function"}; //This works
-
-
     }
 
+    goToProject(id) {
+        this.nav.push(ProjectDetailPage, id);
+    }
 
 
 
