29185dae398b7b8ab5a636960be543968c759bb5
David Zheng <david.zheng@cogeco.com>
2016-05-23 18:14:30 -0400

    add geolocation to map

--- app/pages/map-search/map-search.ts
+++ app/pages/map-search/map-search.ts
@@ -1,4 +1,5 @@
 import {Page, NavController} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 import {ConferenceData} from '../../providers/conference-data';
 
 
@@ -16,42 +17,67 @@ import {ConferenceData} from '../../providers/conference-data';
 export class MapSearchPage {
   private searchQuery: String;
   private items: Array<String>;
+  private map: any;
+  //private latLng: any;
+  private lat: number;
+  private lng: number;
   constructor(private confData: ConferenceData) {
     this.searchQuery = '';
     //this.initializeItems();
     this.items = [];
+
+    //
+
+
+
   }
 
+
+
   onPageLoaded() {
-    this.confData.getMap().then(mapData => {
-      let mapEle = document.getElementById('map');
+    Geolocation.getCurrentPosition().then((resp) => {
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      console.log("Lat:" + resp.coords.latitude + "Lng:" + resp.coords.longitude);
+      this.loadMap(latLng);
+    })
+  }
+  
+  
+  loadMap(LatLng){
+    
+ 
+    //  let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    //let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    //this.confData.getMap().then(mapData => {
+    let mapEle = document.getElementById('map');
 
-      let map = new google.maps.Map(mapEle, {
-        center: mapData.find(d => d.center),
-        zoom: 16
-      });
-
-      mapData.forEach(markerData => {
-        let infoWindow = new google.maps.InfoWindow({
-          content: `<h5>${markerData.name}</h5>`
-        });
-
-        let marker = new google.maps.Marker({
-          position: markerData,
-          map: map,
-          title: markerData.name
-        });
-
-        marker.addListener('click', () => {
-          infoWindow.open(map, marker);
-        });
-      });
-
-      google.maps.event.addListenerOnce(map, 'idle', () => {
-        mapEle.classList.add('show-map');
-      });
-
+    let map = new google.maps.Map(mapEle, {
+      //center: mapData.find(d => d.center),
+      center: LatLng,
+      zoom: 16
     });
+
+    // mapData.forEach(markerData => {
+    //   let infoWindow = new google.maps.InfoWindow({
+    //     content: `<h5>${markerData.name}</h5>`
+    //   });
+
+    //   let marker = new google.maps.Marker({
+    //     position: markerData,
+    //     map: map,
+    //     title: markerData.name
+    //   });
+
+    //   marker.addListener('click', () => {
+    //     infoWindow.open(map, marker);
+    //   });
+    // });
+
+    google.maps.event.addListenerOnce(map, 'idle', () => {
+      mapEle.classList.add('show-map');
+    });
+
+    //});
   }
 
 
