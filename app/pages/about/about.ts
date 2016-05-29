3482411367a6482ba073bbc7f3ca78801f85a063
David Zheng <david.zheng@cogeco.com>
2016-05-29 10:19:08 -0400

    slider and post

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,7 +1,54 @@
-import {Page} from 'ionic-angular';
-
+import {Page, NavController, NavParams, Platform} from 'ionic-angular';
+import {OnInit} from 'angular2/core';
+import {MapleRestData} from '../../providers/maple-rest-data/maple-rest-data';
+import {Post} from './post';
+//import {MAPLECONF} from '../../providers/maple-rest-data/maple-config';
 
 @Page({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/about/about.html',
+ 
 })
-export class AboutPage {}
+export class AboutPage implements OnInit {
+ 
+  private section: string = "about";
+  private isAndroid: boolean = false;
+  // private postAbout = {
+  //   id: '',
+  //   title: ''
+  //  };
+  private postAbout: Object;
+ // private postAbout: Post;
+
+  static get parameters() {
+    return [[MapleRestData]];
+  }
+
+  constructor(private mapleRestData: MapleRestData) {
+   
+  }
+
+
+  ngOnInit() {
+    this.getResult('index.php?r=ngget/getAbout');
+  }
+
+  getResult(url) {
+    this.mapleRestData.load(url, { id: 27 }).subscribe(
+      data => { this.postAbout = data; console.log("About Page Title:" + this.postAbout.title) }
+    );
+    // this.mapleRestData.load(url, {id: 28}).subscribe(
+    //   data => this.postAdvantage = data
+    // );
+    // this.mapleRestData.load(url, {id: 30}).subscribe(
+    //     data => this.postContact = data
+    // );
+    // this.mapleRestData.load(url, {id: 31}).subscribe(
+    //    data => this.postHire = data
+    // );
+  }
+
+
+
+
+
+}
---
+++ app/pages/about/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+  id: number;
+  title: String;
+  content: String;
+  imgHost: String;
+  catname: String;
+
+}
