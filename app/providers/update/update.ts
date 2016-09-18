cb87d86d5f1684661b9b95ca3b328438c58b56fe
Hu <s7058812@bns.bns>
2016-09-17 21:42:38 -0400

    updateService

---
+++ app/providers/update/update.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+import { Deploy } from '@ionic/cloud-angular';
+
+/*
+  Generated class for the Update provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UpdateService {
+
+  constructor(public deploy: Deploy,
+      private alertc: AlertController) {
+  }
+
+  loadNewSnapshotAlert() {
+    let alert = this.alertc.create({
+      title: '软件更新可用',
+      message: '新版本已下载，重启应用？',
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('loadSnapshotAlert Cancel clicked');
+          }
+        },
+        {
+          text: '重启应用',
+          handler: () => {
+            alert.dismiss().then(res => {
+              this.deploy.load();
+            })
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+  loadNewSnapshot() {
+    this.deploy.check().then((snapshotAvailable: boolean) => {
+        if (snapshotAvailable) {
+          this.deploy.download().then(() => {
+            this.deploy.extract().then(() => {
+              this.loadNewSnapshotAlert();
+            });
+          });
+        }
+      });
+ }
+}
+
