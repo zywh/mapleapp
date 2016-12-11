c2e882b960f81e2b420beb9658e68634fe12191a
Zhenghui Hu <hzh@Zhenghuis-MacBook-Pro.local>
2016-12-11 09:57:03 -0500

    add deeperlink for homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,6 +68,7 @@ let storage: Storage = new Storage();
 
 export const deepLinkConfig: DeepLinkConfig = {
   links: [
+    { component: HomePage, name: '主页', segment: 'home' },
     { component: HouseDetailPage, name: '房源详情', segment: 'housedetail/:id' },
     { component: HouseDetailPage, name: '房源详情2', segment: 'housedetail/:id/:VOWtoken' },
     { component: ProjectDetailPage, name: '项目详情', segment: 'projectdetail/:id' },
