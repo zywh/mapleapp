b364ff207e87bbb9b03646fc0e0b4b669aaf43cd
ying zheng <dzheng@yings-iMac.local>
2016-12-19 14:49:42 -0500

    public for viewchild house-detail

--- src/pages/house-detail/house-detail.ts
+++ src/pages/house-detail/house-detail.ts
@@ -29,10 +29,8 @@ import { houseListModel } from '../../models/houseListModel';
 })
 export class HouseDetailPage {
     @ViewChild(Content) content: Content;
-    @ViewChild(HouseDetailViewComponent)
-    private houseDetailView: HouseDetailViewComponent;
-    @ViewChild(MapleMapSearchComponent)
-    private mapleMap: MapleMapSearchComponent;
+    @ViewChild(HouseDetailViewComponent) houseDetailView: HouseDetailViewComponent;
+    @ViewChild(MapleMapSearchComponent) mapleMap: MapleMapSearchComponent;
 
     public isFav = { houseFav: false, routeFav: false };
     public isMore: Boolean = true; //more buttom will be disabled before toast is dismiss
